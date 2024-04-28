7efa759057238ed287015841a574989811a122f2
Gabriel <gawyyyyy@gmail.com>
2024-04-29 00:45:45 +0800

    restored env files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    weatherApi : 'https://api.openweathermap.org/data/2.5/weather?',
+    apiKey : '095d712c0dea6db1d6b7f8b6df51eaa1',
+    directGeocodingApi : 'http://api.openweathermap.org/geo/1.0/direct?q=',
+    reverseGeocodingApi : 'http://api.openweathermap.org/geo/1.0/reverse?',
+    forecastAPI : 'api.openweathermap.org/data/2.5/forecast?'
+};
