f2ff3263c598ace2204e9bedb2bb6cd3b277a14f
Gabriel <gawyyyyy@gmail.com>
2024-04-30 02:05:34 +0800

    add env files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    weatherApi : 'https://api.openweathermap.org/data/2.5/weather?',
+    apiKey : '095d712c0dea6db1d6b7f8b6df51eaa1',
+    directGeocodingApi : 'https://api.openweathermap.org/geo/1.0/direct?q=',
+    reverseGeocodingApi : 'https://api.openweathermap.org/geo/1.0/reverse?',
+    forecastAPI : 'https://api.openweathermap.org/data/2.5/forecast?',
+};
